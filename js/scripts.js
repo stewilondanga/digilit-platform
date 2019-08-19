ca36b84c4df1c2e45c5a42cd36038d088a07fac9
Stewart ilondanga <stilondanga@gmail.com>
2019-08-19 14:10:03 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,63 @@
+$.fn.pageScroller = function(options) {
+
+  var settings = $.extend({
+    nav: $(this),
+    links: $(this).find('a'),
+    navActiveClass: 'active',
+    scrollContainer: $('html, body'),
+    scrollSpeed: 1500,
+    section: $('section'),
+    navHeight: $(this).outerHeight()
+  }, options);
+
+  settings.links.each(function() {
+    var link = $(this),
+      href = link.attr('href'),
+      target = $(href);
+
+    link.on('click', function(e) {
+      e.preventDefault();
+      settings.links.removeClass(settings.navActiveClass);
+      link.addClass(settings.navActiveClass);
+      settings.scrollContainer.stop().animate({
+        scrollTop: target.offset().top - settings.navHeight
+      }, settings.scrollSpeed);
+    });
+
+  });
+
+  $(window).on('scroll', function() {
+    var wScroll = $(window).scrollTop() + settings.navHeight;
+    settings.section.each(function(index) {
+      var id = $(this).attr('id'),
+        top = $(this).offset().top,
+        secHeight = $(this).outerHeight();
+      if (top <= wScroll && (top + secHeight) > wScroll) {
+        settings.links.removeClass(settings.navActiveClass);
+        settings.nav.find('a[href="#' + id + '"]').addClass(settings.navActiveClass);
+      }
+    });
+  });
+};
+
+$(document).ready(function() {
+  $('nav').pageScroller({
+    // nav: $(this),						default - Your navigation
+    // links: $(this).find('a'),			default - The links with the # to scroll to
+    // scrollContainer: $('html, body'),	default - The container you want to scroll
+    // scrollSpeed:		1500,				//default - 1500
+    // section: $('section'),    			default - The elements you want to scroll to with the same hash # as the links
+    // navHeight: $(this).outerHeight		default - The height of the navigation
+    // navActiveClass:		'active',		default - Active class name op links
+  });
+});
+
+
 var navigate = (function() {
-	$('.dd').toggle();
-	$('.dd_btn').click(function() {
-		var dataName = $(this).attr('data-name');
-		$('.dd').hide();
-		$('.' + dataName).toggle();
-	});
+  $('.dd').toggle();
+  $('.dd_btn').click(function() {
+    var dataName = $(this).attr('data-name');
+    $('.dd').hide();
+    $('.' + dataName).toggle();
+  });
 })();
