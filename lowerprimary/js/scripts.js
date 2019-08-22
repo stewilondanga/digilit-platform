5c2a2070bbaaf9b7089d50edad9d9b9cad80d3ec
Stewart ilondanga <stilondanga@gmail.com>
2019-08-22 16:28:58 +0300

    add lowerprimary/css/styles.css

--- lowerprimary/js/scripts.js
+++ lowerprimary/js/scripts.js
@@ -1,8 +1,29 @@
+$('.hitem-indicators li').each(function() {
+  $(this).click(function() {
+    $('.hitem-indicators li').removeClass('active');
+    $(this).addClass('active');
+  })
+});
+
+$(".navbrand button").click(function() {
+  $("nav,#content-wrap").toggleClass("open");
+  $(".navbrand").toggleClass("opened");
+});
+
+$(".navs li a").each(function() {
+  $(this).click(function() {
+    var target = $(this).attr('href');
+    $('html,body').animate({
+      scrollTop: $(target).offset().top
+    }, 1000);
+  });
+});
+
 var navigate = (function() {
-	$('.dd').toggle();
-	$('.dd_btn').click(function() {
-		var dataName = $(this).attr('data-name');
-		$('.dd').hide();
-		$('.' + dataName).toggle();
-	});
+  $('.dd').toggle();
+  $('.dd_btn').click(function() {
+    var dataName = $(this).attr('data-name');
+    $('.dd').hide();
+    $('.' + dataName).toggle();
+  });
 })();
